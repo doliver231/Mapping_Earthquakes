d5388d38ffc046a0c76474a7c64912f361527d9d
doliver231 <doliver91@gmail.com>
2023-01-11 17:26:04 -0500

    Added file

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 // Create the map object with center and zoom level.
 //let map = L.map('mapid').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">MapBox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,15 +20,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -76,7 +76,7 @@ let line = [
 L.polyline(line, {
     color: "yellow"
  }).addTo(map);
- 
+
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/doliver231/Mapping_Earthquakes/Mapping_GeoJSON_Points/Simple_Map/majorAirports.json";
 // Grabbing our GeoJSON data.
